9cd90ceff9f94a3a0a90c63f46aa9af75d0b94f5
jitendra-sr <singh.jitendra.up85@gmail.com>
2025-01-10 00:40:40 +0530

    Trie Implementation

---
+++ String/prefixTree.cpp
@@ -0,0 +1,64 @@
+struct TrieNode{
+    char data; // Key is not necessary for basic trie as node present at specific index works as presence of that character. We can take key as count to keep track of how many words include this node.
+    TrieNode* children[26];
+    bool isTerminal;
+    TrieNode(char ch){
+        data=ch;
+        for(int i=0;i<26;i++) children[i]=nullptr;
+        isTerminal=false;
+    }
+};
+class Trie {
+    TrieNode* root;
+public:
+    Trie() {
+        root=new TrieNode('\0');
+    }
+    
+    void insert(string word) {
+        TrieNode* curr=root;
+        int i=0;
+        while(i<word.size()){
+            int index=word[i]-'a';
+            if(!curr->children[index]){
+                TrieNode* child=new TrieNode(word[i]);
+                curr->children[index]=child;
+            }
+            curr=curr->children[index];
+            i++;
+        }
+        curr->isTerminal=true;
+    }
+    
+    bool search(string word) {
+        TrieNode* curr=root;
+        int i=0;
+        while(i<word.size()){
+            int index=word[i]-'a';
+            if(!curr->children[index]) return false;
+            curr=curr->children[index];
+            i++;
+        }
+        return curr->isTerminal;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* curr=root;
+        int i=0;
+        while(i<prefix.size()){
+            int index=prefix[i]-'a';
+            if(!curr->children[index]) return false;
+            curr=curr->children[index];
+            i++;
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
