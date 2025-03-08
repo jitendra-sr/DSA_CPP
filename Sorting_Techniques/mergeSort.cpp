0ed0f461c70a9f98e09309339cd7da8e3cca2fba
jitendra-sr <singh.jitendra.up85@gmail.com>
2025-03-08 13:03:28 +0530

    Included Invertion Count

--- Sorting_Techniques/mergeSort.cpp
+++ Sorting_Techniques/mergeSort.cpp
@@ -1,11 +1,15 @@
 class Solution {
+    int inversionCount=0;
     void merge(int l, int r, vector<int>& arr){
         int* tmp=new int[r-l+1]; int t=0;
         int mid=(l+r)/2;
         int i=l,j=mid+1;
         while(i<=mid && j<=r){
-            if(arr[i]<arr[j]) tmp[t++]=arr[i++];
-            else tmp[t++]=arr[j++];
+            if(arr[i]<=arr[j]) tmp[t++]=arr[i++];
+            else {
+                inversionCount+=mid-i+1;
+                tmp[t++]=arr[j++];
+            }
         }
         while(i<=mid) tmp[t++]=arr[i++];
         while(j<=r) tmp[t++]=arr[j++];
