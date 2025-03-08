9fad72b0b72fda6b4ebdc8c903b1086bee16b5ae
jitendra-sr <singh.jitendra.up85@gmail.com>
2025-03-08 22:34:18 +0530

    Added some optimizations

--- Sorting_Techniques/mergeSort.cpp
+++ Sorting_Techniques/mergeSort.cpp
@@ -37,3 +37,31 @@ public:
 // 4. Parallel & Distributed Computing (e.g. Bitonic Sort)
 // 6. Sorting Linked Lists
 // 7. Inversion Count Problem
+
+class Solution {
+    void merge(int l, int r, auto& nums){
+        vector<int> tmp(r-l+1); int ind=0;
+        int mid=(l+r)/2;
+        int i=l, j=mid+1;
+        while(i<=mid && j<=r){
+            if(nums[i]<=nums[j]) tmp[ind++]=nums[i++];
+            else tmp[ind++]=nums[j++];
+        }
+        while(i<=mid) tmp[ind++]=nums[i++];
+        while(j<=r) tmp[ind++]=nums[j++];
+        copy(tmp.begin(), tmp.end(), nums.begin() + l);
+    }
+    void mergeSort(int l, int r, auto& nums){
+        if(l>=r) return;
+        int mid=(l+r)/2;
+        mergeSort(l,mid,nums);
+        mergeSort(mid+1,r,nums);
+        // merge(l,r,nums); // Performance efficient
+        inplace_merge(nums.begin()+l,nums.begin()+mid+1,nums.begin()+r+1); // Space efficient
+    }
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(0,nums.size()-1,nums);
+        return nums;
+    }
+};
