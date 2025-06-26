8fc45f4c8f03e65f136bb7b1ffef587f1aacf843
jitendra-sr <singh.jitendra.up85@gmail.com>
2025-06-26 16:57:17 +0530

    Optimised prime check function

--- Maths/prime.cpp
+++ Maths/prime.cpp
@@ -16,13 +16,18 @@ public:
         return cnt;
     }
 };
+
+
+// Check for factors of the form 6k ± 1
+// This is because all primes greater than 3 can be expressed in this form.
 bool isPrime(int n) {
-    if (n <= 1) return false;
-    if (n == 2) return true;
-    if (n % 2 == 0) return false;
-    int limit = static_cast<int>(sqrt(n));
-    for (int i = 3; i <= limit; i += 2) {
-        if (n % i == 0) return false;
+    if (n<=1) return false;
+    if (n<=3) return true;
+    if (n % 2==0||n % 3==0) return false;
+
+    for (int i=5; i*i<=n; i+=6) { 
+        if (n%i ==0||n%(i+2) == 0)
+            return false;
     }
     return true;
 }
