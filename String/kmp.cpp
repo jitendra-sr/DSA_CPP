4f73b67db2246a8d5ceeb23623ae5af50a946304
jitendra-sr <singh.jitendra.up85@gmail.com>
2025-07-01 00:53:44 +0530

    Modified prefix function

--- String/kmp.cpp
+++ String/kmp.cpp
@@ -1,3 +1,49 @@
+// https://cp-algorithms.com/string/prefix-function.html
+vector<int> prefixArray(const string& pat){
+    int m = pat.size();
+    vector<int> lps(m, 0);
+
+    for (int i = 1; i < m; i++) {
+        int j = lps[i - 1]; // Length of the previous longest prefix suffix
+        while (j > 0 && pat[j] != pat[i]) {
+            j = lps[j - 1]; // trying to find a valid shorter prefix suffix
+        }
+        if (pat[i] == pat[j]) {
+            j++; // If characters match, increment the length of the current longest prefix suffix
+        }
+        lps[i] = j; // Store the length of the longest prefix suffix for the current index
+    }
+    return lps;
+}
+void kmpSearch(const string& text, const string& pattern) {
+    int n = text.length();
+    int m = pattern.length();
+
+    vector<int> lps = prefixArray(pattern); // Get the LPS array for the pattern
+    int i = 0;  // index for text
+    int j = 0;  // index for pattern
+
+    while (i < n) {
+        if (pattern[j] == text[i]) {
+            i++;
+            j++;
+        }
+        else {
+            if (j != 0) { // Use LPS to avoid unnecessary comparisons
+                j = lps[j - 1]; 
+            }
+            else {
+                i++;
+            }
+        }
+        if (j == m) {
+            // Found a match, print the starting index
+            cout << "Pattern found at index " << i - j << endl;
+            if(j != 0) j = lps[j - 1]; // Use LPS to skip unnecessary comparisons
+        }
+    }
+}
+
 vector<int> buildLpsArray(const string& pattern) {
     int m = pattern.length();
     vector<int> lps(m);
@@ -23,32 +69,4 @@ vector<int> buildLpsArray(const string& pattern) {
         }
     }
     return lps;
-}
-void kmpSearch(const string& text, const string& pattern) {
-    int n = text.length();
-    int m = pattern.length();
-
-    vector<int> lps = buildLPSArray(pattern); // Build the LPS array for the pattern
-    int i = 0;  // index for text
-    int j = 0;  // index for pattern
-
-    while (i < n) {
-        if (pattern[j] == text[i]) {
-            i++;
-            j++;
-        }
-        else {
-            if (j != 0) { // Use LPS to avoid unnecessary comparisons
-                j = lps[j - 1]; 
-            }
-            else {
-                i++;
-            }
-        }
-        if (j == m) {
-            // Found a match, print the starting index
-            cout << "Pattern found at index " << i - j << endl;
-            if(j != 0) j = lps[j - 1]; // Use LPS to skip unnecessary comparisons
-        }
-    }
 }
