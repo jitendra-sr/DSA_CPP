ed71df50d4fd06328a53ec1df5b7161f98235ed6
jitendra-sr <singh.jitendra.up85@gmail.com>
2025-09-14 00:54:13 +0530

    modified cp template

--- cp.cpp
+++ cp.cpp
@@ -1,17 +1,10 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 
-int n,m;
-map<char,pair<int,int>> dir = {{'U',{-1,0}}, {'D',{1,0}}, {'R',{0,1}}, {'L',{0,-1}}};
-
 int main(){
-    cin>>n>>m;
-    int k; cin>>k;
-    vector<vector<char>> grid(n, vector<char>(m));
-    vector<vector<bool>> vis(n, vector<bool>(m));
-
-    
+    ll t; cin >> t;
+    while(t--){}
 
     return 0;
 }
